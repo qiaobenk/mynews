3b6e8eee60b9147c6e13a9506b76b7593cc3f98c
qiaobenk <89115512+qiaobenk@users.noreply.github.com>
2021-09-16 15:49:05 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+ 
+Route::get('xxx', 'Admin\NewsController@add');
+ 
 Route::get('/', function () {
-    return view('welcome');
-});
+returnview('welcome');
+    });
+Route::group(['prefix' => 'admin'], function() {
+Route::get('news/create', 'AAAController@bbb');
+Route::get('profile/create', 'Admin\NewsController@add');
+Route::get('news/edit', 'Admin\NewsController@edit');
+    });
