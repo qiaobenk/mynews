8414b9bb1949de43a32473c54e20141668aae379
qiaobenk <89115512+qiaobenk@users.noreply.github.com>
2021-09-30 15:22:27 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {​​​​​​
 returnview('welcome');
     }​​​​​​);
 Route::group(['prefix' => 'admin'], function() {​​​​​​
-Route::get('news/create', 'AAAController@bbb');
+Route::get('news/create', 'Admin\NewsController@add');
 Route::get('profile/create', 'Admin\NewsController@add');
 Route::get('news/edit', 'Admin\NewsController@edit');
+Route::get('profile/edit', 'ProfileController@edit');
     }​​​​​​);
 
